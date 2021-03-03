5fdd1a2156bf5662f866c0764478146dc38c397c
Edson <edsonrobles@outlook.com>
2021-03-02 21:06:47 -0600

    Proyecto SvelteJS

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,38 @@
 <script>
-    import Home from './page/Home.svelte';
+	import Home from './page/Home.svelte';
 
-    import { Router, Route, Link, link } from 'svelte-routing';
+	import { Router, Route, Link, link } from 'svelte-routing';
 </script>
 
 <Home />
+<main>
+	<div class="image">
+		<img src="" alt="" />
+	</div>
+	<h1>Hola <span>Mundo</span></h1>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius magni consequatur fugiat laboriosam porro!
+		Voluptates eos, ad quae expedita earum placeat nesciunt, qui magnam tenetur minus nam exercitationem recusandae
+		odio adipisci iste? Cum dicta, nisi alias odio excepturi facilis, fugit sit hic officia quia, libero repellat.
+		Maxime accusamus eaque perferendis in beatae sequi veritatis nisi aut facere, quam possimus voluptates et nobis
+		expedita nesciunt blanditiis dolore repellat ipsa assumenda laudantium, dicta debitis. Magni accusantium
+		asperiores voluptates inventore numquam repellendus suscipit, perspiciatis eligendi tempore doloremque neque
+		temporibus itaque sunt deleniti possimus dolorum illum, tenetur distinctio? Sit exercitationem repellat
+		voluptatum magnam omnis impedit officiis qui necessitatibus quis libero. Culpa, incidunt reiciendis quae dolore
+		sed a quam error cumque beatae, officiis ducimus dolorem fuga? Incidunt adipisci corporis fugiat, inventore
+		totam tempora illo praesentium molestias. Eveniet sequi nobis a repudiandae dolorum enim maxime nisi! Deserunt
+		in atque nihil possimus blanditiis? Vitae expedita eaque nostrum quo blanditiis officia quibusdam cum. Commodi
+		sequi, adipisci modi sapiente tempore vero quibusdam cumque explicabo, maxime molestias ullam expedita magnam
+		saepe incidunt dolorem quaerat molestiae dicta. Eum veritatis dicta maxime ullam? Repellat tempore laboriosam,
+		doloremque quidem placeat quisquam, vero reiciendis quasi veritatis vel atque rerum necessitatibus molestias
+		dolorum impedit dolore?
+	</p>
+</main>
 
 <style lang="scss">
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
